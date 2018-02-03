4a15579be3cb9aea9ca59939d71575839c03aa59
MikeDCastillo <mikecastillouvu@gmail.com>
2018-02-03 01:44:52 -0700

    Adds bridging header into proj for Tesseract
    Tesseract was written in objective c, so going forward we use a bridging header to give it the objective c wrapper

---
+++ APPdentify/Dummy.h
@@ -0,0 +1,13 @@
+//
+//  Dummy.h
+//  APPdentify
+//
+//  Created by Michael Castillo on 2/3/18.
+//  Copyright © 2018 Michael Castillo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dummy : NSObject
+
+@end
---
+++ APPdentify/Dummy.m
@@ -0,0 +1,13 @@
+//
+//  Dummy.m
+//  APPdentify
+//
+//  Created by Michael Castillo on 2/3/18.
+//  Copyright © 2018 Michael Castillo. All rights reserved.
+//
+
+#import "Dummy.h"
+
+@implementation Dummy
+
+@end
